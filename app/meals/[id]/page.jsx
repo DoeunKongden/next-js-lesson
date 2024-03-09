7842d9797626c665ae7bcd881b0ce98db3eeae78
Doeun Kongden <doeunkongden1@gmail.com>
2024-03-09 15:35:38 +0700

    finish with main page

---
+++ app/meals/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MealsDetailPage({ params }) {
+  return (
+    <div className='min-h-screen'>
+      <h1>Meals ID :{params.id}</h1>
+    </div>
+  )
+}
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShareMealPage() {
+    return (
+        <div className='min-h-screen'>
+            <h1>Share Meals</h1>
+        </div>
+    )
+}
